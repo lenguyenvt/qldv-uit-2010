30fc8035c90f189655f1fd278e2ef19b666c3d82
thenightofsorrows <thenightofsorrows@97d6d196-4139-c0a4-be54-d55241d34ae9>
2010-07-28 02:07:05 +0000

    fix 2 bugs Quoc had been decribed

--- trunk/ qldv-uit-2010 --username thenightofsorrows/qldv/config.php
+++ trunk/ qldv-uit-2010 --username thenightofsorrows/qldv/config.php
@@ -9,12 +9,12 @@ if(isset($_SERVER['HTTP_REFERER']) && strstr($_SERVER['HTTP_REFERER'],".swf"))
 session_start();
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASSWORD", "mysql");
-define("DB_NAME", "qldv2");
+define("DB_PASSWORD", "123456");
+define("DB_NAME", "qldv");
 define("DB_PREFIX", "");
 
-$sub_server="/qldv2/images";
-$base_url="/qldv2";
+$sub_server="/qldv/images";
+$base_url="/qldv";
 
 $db = new db();
 $db->connect();
