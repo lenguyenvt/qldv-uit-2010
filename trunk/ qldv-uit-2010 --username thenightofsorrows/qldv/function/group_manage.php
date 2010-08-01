aecd75f3ddca800fac463bc1b12db61ce0c698f1
khuongcomputer <khuongcomputer@97d6d196-4139-c0a4-be54-d55241d34ae9>
2010-08-01 09:05:46 +0000

    Sửa một số hàm và gần xong phần quản lý cơ sở đoàn.

--- trunk/ qldv-uit-2010 --username thenightofsorrows/qldv/function/group_manage.php
+++ trunk/ qldv-uit-2010 --username thenightofsorrows/qldv/function/group_manage.php
@@ -33,15 +33,15 @@ function page_content() {
 					}
 				}
 			}
-		$sql = "SELECT `id_cosodoan`,`ten`,`parent`,`cap`,`co_dau` FROM `cosodoan` WHERE " . get_cosodoan_capduoi ( $user ['id_doanvien'], "`id_cosodoan`" );
-		$db->query ( $sql );
+		$sql = "SELECT `id_cosodoan`,`ten`,`parent`,`co_dau` FROM `cosodoan` WHERE " . get_cosodoan_capduoi($user['id_doanvien'], "`id_cosodoan`" );
+		$db->query ($sql);
 		$i = 0;
 		$danhsachcosodoan = array ();
 		while ( $tmp = mysql_fetch_array ( $db->query_result ) ) {
-			$danhsachcosodoan [$i ++] = $tmp;
+			$danhsachcosodoan [$i++] = $tmp;
 		}
 		return user_main_form ( group_manage_form ( $danhsachcosodoan ) );
 	} else
-		return user_main_form ( page_error ( "B&#7841;n kh&#244;ng c&#243; quy&#7873;n truy c&#7853;p trang n&#224;y!" ) );
+		return user_main_form(page_error("B&#7841;n kh&#244;ng c&#243; quy&#7873;n truy c&#7853;p trang n&#224;y!"));
 }
 ?>
